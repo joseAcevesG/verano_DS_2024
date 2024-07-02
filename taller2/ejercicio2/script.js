0fb507ac9d1bc02c86cdd5e0a733df4b7bba31f0
joseAcevesG <jose.aceves@iteso.mx>
2024-07-01 19:16:51 -0600

    Refactor code for ejercicio1 and ejercicio2 scripts. clean exercises

--- taller2/ejercicio2/script.js
+++ taller2/ejercicio2/script.js
@@ -4,44 +4,10 @@ const parrafo = document.getElementById("parrafo");
 
 let rectangulo = new Rectangulo();
 
-function crearNodos() {
-	for (let i = 0; i < 5; i++) {
-		let nodeTemp = document.createElement("p");
-		padre.append(nodeTemp);
-		nodeTemp.innerHTML = i + 1;
-	}
-}
+function crearNodos() {}
 
-function quitarNodo() {
-	while (padre.firstElementChild != null) {
-		padre.firstElementChild.remove();
-	}
+function quitarNodo() {}
 
-	while (padre2.firstElementChild != null) {
-		padre2.firstElementChild.remove();
-	}
-}
+function listaComida() {}
 
-function listaComida() {
-	let comidas = ["sopa", "carne", "manzana", "pan"];
-
-	for (let i = 0; i < comidas.length; i++) {
-		console.log("hola");
-		let nodeTemp = document.createElement("p");
-		padre2.append(nodeTemp);
-		nodeTemp.innerHTML = comidas[i];
-	}
-}
-
-function Rectangulo() {
-	this.altura = 10;
-	this.base = 20;
-
-	this.area = function () {
-		parrafo.innerHTML = "el area es " + this.altura * this.base;
-	};
-
-	this.perimetro = function () {
-		parrafo.innerHTML = "el perimetro es " + (this.altura * 2 + this.base * 2);
-	};
-}
+function Rectangulo() {}
